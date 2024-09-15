5439c622ab45d159cacb5d76f86ca30609b13a88
Nicolas Duque Gutierrez <n.duque1@utp.edu.co>
2024-09-15 15:13:24 -0500

    Hello world

---
+++ hello-world/target/debug/deps/hello_world-43afdd4e4ab6397e.d
@@ -0,0 +1,5 @@
+/Users/mesopotamico/Desktop/projects/the-book/hello-world/target/debug/deps/hello_world-43afdd4e4ab6397e: src/main.rs
+
+/Users/mesopotamico/Desktop/projects/the-book/hello-world/target/debug/deps/hello_world-43afdd4e4ab6397e.d: src/main.rs
+
+src/main.rs:
---
+++ hello-world/target/debug/hello-world.d
@@ -0,0 +1,1 @@
+/Users/mesopotamico/Desktop/projects/the-book/hello-world/target/debug/hello-world: /Users/mesopotamico/Desktop/projects/the-book/hello-world/src/main.rs
